9ce711a83f7a36824002734f7f620f434ab61253
Alberto Chamorro <albertchc04@gmail.com>
2017-12-25 20:55:49 -0600

    view show detail place - implements breadcrumbs bootstrap

--- src/app/place-details/place-detail.component.ts
+++ src/app/place-details/place-detail.component.ts
@@ -6,8 +6,26 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './place-detail.component.html'
 })
 export class PlaceDetailComponent {
+    places: any = [
+        {id: 1, name: 'Floreria la Gardenia', description: 'es un negocio abierto', plan: 'pagado',
+            closeTime: 1, distance: 1,  active: true},
+        {id: 2, name: 'Donas las pasaditas', description: 'es un negocio dinamico', plan: 'gratuito',
+            closeTime: 3, distance: 1.8, active: false},
+        {id: 3, name: 'Veterinaria huellitas felices', description: 'no se encontro referencia', plan: 'gratuito',
+            closeTime: 2, distance: 2, active: true}
+    ];
+    id = null;
+    place: any = {};
+
     constructor(private route: ActivatedRoute) {
-        console.log('params - ', this.route.snapshot.params['id']);
+        this.id = this.route.snapshot.params['id'];
+        console.log('params - ', this.id);
         console.log('query strings - ', this.route.snapshot.queryParams['action']);
+        this.place = this.findPlace();
+        console.log('place find -> ', this.place);
+    }
+
+    findPlace() {
+        return this.places.filter((place) => { return place.id == this.id })[0] || null;
     }
 }
